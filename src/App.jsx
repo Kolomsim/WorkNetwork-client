1eaeb041025110ba5d3d42c297f263cea11c173b
Kolomiets Maxim <gmkolomiets@gmail.com>
2024-07-10 20:02:41 +0400

    Вынес layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
+// App.js
 import React, { useState, useEffect } from 'react'
-import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	Navigate,
-} from 'react-router-dom'
-import RegisterPage from './pages/LoginSignup/Signup.jsx'
-import LoginPage from './pages/LoginSignup/Login.jsx'
-import ChatPage from './pages/Chat/Chat.jsx'
-import UserPage from './pages/UserProfile/userProfile.jsx'
-import ProfilePage from './pages/UserProfile/userProfile.jsx'
-import Layout from './components/Layout/Layout.jsx'
-import ListOfGroups from './components/ListOfGroups/ListOfGroups.jsx'
-import GroupPage from './components/Group/Group.jsx'
-import ForumsPage from './components/ListOfForum/ListOfForum.jsx'
-import TopicPage from './components/ForumTopic/Topic.jsx'
-import JobSearch from './components/JobFound/JobFound.jsx'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Layout from './components/Layout/Layout'
+import RegisterPage from './pages/RegisterPage'
+import LoginPage from './pages/LoginPage'
+import ChatPage from './pages/ChatPage'
+import UserPage from './pages/UserPage'
+import ListOfGroups from './pages/ListOfGroups'
+import JobSearch from './pages/JobSearch'
+import GroupPage from './pages/GroupPage'
+import ForumsPage from './pages/ForumsPage'
+import ProfilePage from './pages/ProfilePage'
+import TopicPage from './pages/TopicPage'
 
 function App() {
 	const [authenticated, setAuthenticated] = useState(false)
@@ -37,60 +33,60 @@ function App() {
 	}
 
 	return (
-		<Layout>
-			<Routes>
-				<Route path='/signup' element={<RegisterPage />} />
-				<Route
-					path='/login'
-					element={<LoginPage setAuthenticated={setAuthenticated} />}
-				/>
+		<Routes>
+			<Route path='/signup' element={<RegisterPage />} />
+			<Route
+				path='/login'
+				element={<LoginPage setAuthenticated={setAuthenticated} />}
+			/>
+			<Route
+				path='/*'
+				element={<ProtectedRoutes authenticated={authenticated} />}
+			/>
+		</Routes>
+	)
+}
 
-				<Route
-					path='/chats'
-					element={authenticated ? <ChatPage /> : <Navigate to='/login' />}
-				/>
-				<Route
-					path='/user/:username'
-					element={authenticated ? <UserPage /> : <Navigate to='/login' />}
-				/>
-				<Route
-					path='/groups'
-					element={authenticated ? <ListOfGroups /> : <Navigate to='/login' />}
-				/>
-				<Route path='/job-search' element={<JobSearch />} />
-				<Route
-					path='/groups/:id'
-					element={authenticated ? <GroupPage /> : <Navigate to='/login' />}
-				/>
-				<Route
-					path='/forum'
-					element={authenticated ? <ForumsPage /> : <Navigate to='/login' />}
-				/>
-				<Route
-					path='/profile'
-					element={authenticated ? <ProfilePage /> : <Navigate to='/login' />}
-				/>
-				<Route
-					path='/forums/:forumId'
-					element={authenticated ? <TopicPage /> : <Navigate to='/login' />}
-				/>
-				<Route
-					path='*'
-					element={
-						authenticated ? <Navigate to='/chats' /> : <Navigate to='/login' />
-					}
-				/>
-			</Routes>
+function ProtectedRoutes({ authenticated }) {
+	return (
+		<Layout>
+			<Route
+				path='/chats'
+				element={authenticated ? <ChatPage /> : <Navigate to='/login' />}
+			/>
+			<Route
+				path='/user/:username'
+				element={authenticated ? <UserPage /> : <Navigate to='/login' />}
+			/>
+			<Route
+				path='/groups'
+				element={authenticated ? <ListOfGroups /> : <Navigate to='/login' />}
+			/>
+			<Route path='/job-search' element={<JobSearch />} />
+			<Route
+				path='/groups/:id'
+				element={authenticated ? <GroupPage /> : <Navigate to='/login' />}
+			/>
+			<Route
+				path='/forum'
+				element={authenticated ? <ForumsPage /> : <Navigate to='/login' />}
+			/>
+			<Route
+				path='/profile'
+				element={authenticated ? <ProfilePage /> : <Navigate to='/login' />}
+			/>
+			<Route
+				path='/forums/:forumId'
+				element={authenticated ? <TopicPage /> : <Navigate to='/login' />}
+			/>
+			<Route
+				path='*'
+				element={
+					authenticated ? <Navigate to='/chats' /> : <Navigate to='/login' />
+				}
+			/>
 		</Layout>
 	)
 }
 
-function AppWrapper() {
-	return (
-		<Router>
-			<App />
-		</Router>
-	)
-}
-
-export default AppWrapper
+export default App
